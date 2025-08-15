b13d7b9b3e51d86144c5bfee5fb28652c26ff00c
amant2312 <amant2312@gmail.com>
2025-08-15 15:48:59 +0530

    Using JSX to create and render the element.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const heading= React.createElement('h1',{id: 'heading'},"Hello World");
+import React from "react";
+import ReactDOM from "react-dom/client";
+//const heading= React.createElement('h1',{id: 'heading'},"Hello World");
+const heading = <h1 id="heading">Hello World</h1>;
 const root= ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
