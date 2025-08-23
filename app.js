baca6a9ac98092565b148c0fce9f087607934bc7
amant2312 <amant2312@gmail.com>
2025-08-23 16:08:13 +0530

    Header and Body section using React function components (static cards)

--- app.js
+++ app.js
@@ -1,6 +1,80 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//const heading= React.createElement('h1',{id: 'heading'},"Hello World");
-const heading = <h1 id="heading">Hello World</h1>;
+
+/*
+Header 
+    logo
+    nav items
+
+
+Body
+    search
+    card
+
+Footer
+
+*/
+
+const Card= ()=>{
+    return (
+        <div className="card">
+            <img alt="res-image" className="card-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMakWahGD-YlPQJ_7vuuALnNiDQl6TmO2X9j4QfSkjptpnYr69UE4r49E&s" />
+            <h3>Name</h3>
+            <h3>Cusine</h3>
+            <h3>Rating</h3>
+            <h3>Avg order delivery time</h3>
+        </div>
+    );
+}
+
+const Header= ()=>{ 
+    return (
+    <div className='header'>
+        <div className= "logo-container">
+            <img className="logo" src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg" />
+        </div>
+        <div className= "nav-items">
+            <ul >
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+        
+    </div>
+    );
+}
+
+const Body= ()=>{
+    return (
+        <div className="body">
+            <h1>search</h1>
+        <div className= "res-card">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+        </div>
+    );
+}
+
+
+const AppLayout = ()=>{
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+
+    );
+}
+
 const root= ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<AppLayout/>);
